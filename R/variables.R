cbbd76b5cfaf18487eeb1ec331552dca0e474e79
Herb Susmann <herbps10@gmail.com>
2015-12-30 12:42:55 -0500

    Added rudimentary unit extraction to the comprehensive variable list download code, and added explicit import of xml2's read_html

--- R/variables.R
+++ R/variables.R
@@ -1,9 +1,10 @@
 #' Load the NHANES comprehensive variable list
 #'
 #' @param destination where to save the variable list
-#' @param cache whether to cache the downloaded variable list so it doesn't have to be redownloaded every time
+#' @param cache whether to cache the downloaded variable list so it doesn't have to be re-downloaded every time
 #'
 #' @import rvest
+#' @importFrom xml2 read_html
 #' @export
 nhanes_variables <- function(destination = tempfile(), cache = TRUE) {
   if(!dir.exists(dirname(destination))) {
@@ -27,6 +28,19 @@ nhanes_variables <- function(destination = tempfile(), cache = TRUE) {
 
   # Add a cycle column
   dat$Cycle <- paste(dat$BeginYear, dat$EndYear, sep = "-")
+
+  # Extract units (when available) into a separate column
+  # TODO: think about how to fix this, right now it works but it picks up a lot of non-unit things
+  units <- regmatches(dat$VariableDescription, gregexpr("\\((.*?)\\)", dat$VariableDescription))
+  units[sapply(units, length) == 0] <- NA # Replace blanks with NAs
+  units[dat$Component != 'Laboratory' & dat$Component != 'Examination'] <- NA # Replace anything picked up in the non-lab variables as NAs
+  units <- Map(function(x) tail(x, 1), units)
+  units <- unlist(units)
+  units <- Map(function(x) substr(x, 2, nchar(x) - 1), units)
+
+
+  dat$Unit <- unlist(units)
+
 
   return(dat)
 }
