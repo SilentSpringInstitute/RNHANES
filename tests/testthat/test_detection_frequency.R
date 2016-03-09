03e30278d19e48fe634b2c061c58660b3588c623
Herb Susmann <herbps10@gmail.com>
2016-03-09 15:39:01 -0500

    Updated tests for detection frequency and quantile code

--- tests/testthat/test_detection_frequency.R
+++ tests/testthat/test_detection_frequency.R
@@ -1,15 +1,13 @@
 test_that("Passes spot check on raw data", {
-  dat <- nhanes_load_data("PFC", "2011-2012")
-  freqs <- nhanes_detection_frequency(dat, c("LBDPFUAL", "LBDPFDOL"))
+  dat <- nhanes_load_data("PFC", "2011-2012", demographics = TRUE)
+  freqs <- nhanes_detection_frequency(dat, c("LBXPFUA", "LBXPFDO"), c("LBDPFUAL", "LBDPFDOL"))
 
-  expect_equal(freqs$LBDPFUAL, 0.6239496, tolerance = 1e-3)
-  expect_equal(freqs$LBDPFDOL, 0.09401261, tolerance = 1e-3)
+  expect_equal(freqs$value, c(0.5926, 0.0908), tolerance = 1e-3)
 })
 
 test_that("Passes spot check on recoded data", {
-  dat <- nhanes_load_data("PFC", "2011-2012", recode = TRUE)
-  freqs <- nhanes_detection_frequency(dat, c("LBDPFUAL", "LBDPFDOL"))
+  dat <- nhanes_load_data("PFC", "2011-2012", demographics = TRUE, recode = TRUE)
+  freqs <- nhanes_detection_frequency(dat, c("LBXPFUA", "LBXPFDO"), c("LBDPFUAL", "LBDPFDOL"))
 
-  expect_equal(freqs$LBDPFUAL, 0.6239496, tolerance = 1e-3)
-  expect_equal(freqs$LBDPFDOL, 0.09401261, tolerance = 1e-3)
+  expect_equal(freqs$value, c(0.5926, 0.0908), tolerance = 1e-3)
 })
--- tests/testthat/test_quantile.R
+++ tests/testthat/test_quantile.R
@@ -2,7 +2,35 @@ nhanes_data <- nhanes_load_data("PFC", "2011-2012", demographics = TRUE)
 
 test_that("Passes spot check", {
 
-  quantiles <- nhanes_quantile(nhanes_data, column = "LBXPFUA", weights_column = "WTSA2YR", quantiles = c(0, 0.5, 0.75, 0.99, 1))
+  quantiles <- nhanes_quantile(nhanes_data, column = "LBXPFUA", comment_column = "LBDPFUAL", weights_column = "WTSA2YR", quantiles = c(0, 0.5, 0.75, 0.99, 1))
 
-  expect_equivalent(quantiles, c(0.07, 0.12, 0.22, 1.45, 6.96))
+  expect_equal(quantiles$below_lod, c(TRUE, FALSE, FALSE, FALSE, FALSE))
+  expect_equivalent(quantiles$value, c(0.07, 0.12, 0.22, 1.45, 6.96))
+})
+
+test_that("Quantiles can be computed for multiple chemicals at a time", {
+  quantiles <- nhanes_quantile(nhanes_data,
+                               column = c("LBXPFUA", "LBXPFSA"),
+                               comment_column = c("LBDPFUAL", "LBDPFSAL"),
+                               weights_column = c("WTSA2YR", "WTSA2YR"),
+                               quantiles = c(0, 0.5, 0.75, 0.99, 1))
+
+  expect_equivalent(quantiles$value, c(0.07, 0.12, 0.22, 1.45, 6.96, 0.07, 0.07, 0.07, 0.07, 0.62))
+  expect_equal(quantiles$column, c(rep("LBXPFUA", 5), rep("LBXPFSA", 5)))
+})
+
+test_that("Quantiles can be computed for multiple chemicals at a time, without specifying weights", {
+  quantiles <- nhanes_quantile(nhanes_data,
+                               column = c("LBXPFUA", "LBXPFSA"),
+                               comment_column = c("LBDPFUAL", "LBDPFSAL"),
+                               quantiles = c(0, 0.5, 0.75, 0.99, 1))
+
+  expect_equivalent(quantiles$value, c(0.07, 0.12, 0.22, 1.45, 6.96, 0.07, 0.07, 0.07, 0.07, 0.62))
+  expect_equal(quantiles$column, c(rep("LBXPFUA", 5), rep("LBXPFSA", 5)))
+})
+
+#nhanes_data <- nhanes_load_data(c())
+
+test_that("Quantiles can be computed for chemicals in multiple files", {
+
 })
