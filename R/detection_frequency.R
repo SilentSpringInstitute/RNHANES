544aa88242b80eb0c090ec1b672eaf344423a4fe
Herb Susmann <herbps10@gmail.com>
2016-03-10 11:37:01 -0500

    Updated documentation

--- R/detection_frequency.R
+++ R/detection_frequency.R
@@ -1,18 +1,20 @@
 #' Compute detection frequencies of NHANES data
 #'
 #' @param nhanes_data data frame containing NHANES data
-#' @param columns comment column names of the variables to compute detection frequencies for
-#'
+#' @param column column names of the variables to compute detection frequencies for
+#' @param comment_column comment column names of the variables to compute detection frequencies for
+#' @param weights_column sample weight column
 #' @return named vector of detection frequencies
 #'
 #'
 #' @examples
 #'
-#' dat <- load_nhanes_data("UHG_G", "2011-2012", demographics = TRUE)
+#' \dontrun{
+#' dat <- nhanes_load_data("UHG_G", "2011-2012", demographics = TRUE)
 #'
 #' # Compute detection frequency
-#' nhanes_detection_frequency(dat, c("URXUHG"))
-#'
+#' nhanes_detection_frequency(dat, c("URXUHG"), c("URDUHGLC"))
+#' }
 #'
 #' @export
 nhanes_detection_frequency <- function(nhanes_data, column, comment_column, weights_column = "") {
