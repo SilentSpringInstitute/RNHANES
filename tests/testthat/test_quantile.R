5e78c06dab1347785ce75b50a21268c4bc387807
Herb Susmann <herbps10@gmail.com>
2016-05-04 11:42:23 -0400

    Added more quantile spot checks

--- tests/testthat/test_quantile.R
+++ tests/testthat/test_quantile.R
@@ -4,10 +4,18 @@ test_that("Computing quantiles works", {
   nhanes_data <- nhanes_load_data("PFC", "2011-2012", demographics = TRUE, cache = TRUE)
 
   test_that("Passes spot check", {
-    quantiles <- nhanes_quantile(nhanes_data, column = "LBXPFUA", comment_column = "LBDPFUAL", weights_column = "WTSA2YR", quantiles = c(0, 0.5, 0.75, 0.99, 1))
+    quantiles <- nhanes_quantile(nhanes_data, column = "LBXPFUA", comment_column = "LBDPFUAL", weights_column = "WTSA2YR", quantiles = c(0, 0.5, 0.75, 0.90, 0.95))
 
     expect_equal(quantiles$below_lod, c(TRUE, FALSE, FALSE, FALSE, FALSE))
-    expect_equivalent(quantiles$value, c(0.07, 0.12, 0.22, 1.45, 6.96))
+    expect_equivalent(quantiles$value, c(0.07, 0.12, 0.22, 0.38, 0.62))
+
+    quantiles <- nhanes_quantile(nhanes_data, "LBXPFOS", "LBDPFOSL", weights_column = "WTSA2YR", quantiles = c(0.5, 0.75, 0.9, 0.95))
+    expect_equal(quantiles$below_lod, c(FALSE, FALSE, FALSE, FALSE))
+    expect_equivalent(quantiles$value, c(6.53, 10.5, 15.7, 21.7))
+
+    quantiles <- nhanes_quantile(nhanes_data, "LBXPFHP", "LBDPFHPL", weights_column = "WTSA2YR", quantiles = c(0.5, 0.75, 0.9, 0.95))
+    expect_equal(quantiles$below_lod, c(TRUE, TRUE, FALSE, FALSE))
+    expect_equivalent(quantiles$value, c(0.07, 0.07, 0.14, 0.22))
   })
 
   test_that("Quantiles can be computed for multiple chemicals at a time", {
