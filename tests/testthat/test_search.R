0142d98761dfc6d3a4f6f3acd7e02a106a488f4f
Herb Susmann <herbps10@gmail.com>
2016-09-08 16:21:17 -0400

    Fixed nhanes_search test -- old one was failing because new PFC data was released for 2013-2014

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -17,8 +17,8 @@ test_that("nhanes_search works", {
   test_that("nhanes_search on files passes spot check", {
     results <- nhanes_search(files, "Polyfluoroalkyl")
 
-    expect_equal(results$data_file_name, c("L24PFC_C", "PFC_D", "PFC_E", "PFC_F", "PFC_G", "PFC_POOL"))
-    expect_equal(results$cycle, c("2003-2004", "2005-2006", "2007-2008", "2009-2010", "2011-2012", "2001-2002"))
+    expect_equal(results$data_file_name, c("SSPFAS_H", "PFAS_H", "L24PFC_C", "PFC_D", "PFC_E", "PFC_F", "PFC_G", "PFC_POOL"))
+    expect_equal(results$cycle, c("2013-2014", "2013-2014", "2003-2004", "2005-2006", "2007-2008", "2009-2010", "2011-2012", "2001-2002"))
   })
 
   test_that("fuzzy search works on variables", {
@@ -30,7 +30,7 @@ test_that("nhanes_search works", {
   test_that("fuzzy search works on files", {
     results <- nhanes_search(files, "fluoro", fuzzy = TRUE)
 
-    expect_equal(nrow(results), 10)
-    expect_equal(results$data_file_name, c("SSANA_A", "SSANA_B", "SSANA_C", "SSPFC_A", "L24PFC_C", "PFC_D", "PFC_E", "PFC_F", "PFC_G", "PFC_POOL"))
+    expect_equal(nrow(results), 14)
+    expect_equal(results$data_file_name, c("SSANA_A", "SSANA_B", "SSANA_C", "FLDEP_H", "FLDEW_H", "SSPFAS_H", "PFAS_H", "SSPFC_A", "L24PFC_C", "PFC_D", "PFC_E", "PFC_F", "PFC_G", "PFC_POOL"))
   })
 })
